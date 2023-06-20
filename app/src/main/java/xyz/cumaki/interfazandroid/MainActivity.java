0f4c05457b25fe3dc8483c585a5685943f90a2a2
bryan robalino <eduardochuez_2003@outlook.com>
2023-06-20 15:44:41 -0500

    se agregaron validaciones en los editText

--- app/src/main/java/xyz/cumaki/interfazandroid/MainActivity.java
+++ app/src/main/java/xyz/cumaki/interfazandroid/MainActivity.java
@@ -2,6 +2,11 @@ package xyz.cumaki.interfazandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText editText = findViewById(R.id.txtNombre);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            public void afterTextChanged(Editable s) {
+                String text = s.toString();
+                String upperCaseText = text.toUpperCase();
+                editText.removeTextChangedListener(this);
+                editText.setText(upperCaseText);
+                editText.setSelection(upperCaseText.length());
+                editText.addTextChangedListener(this);
+            }
+        });
+
+
+
     }
+
+
+
+
+
+
 }
